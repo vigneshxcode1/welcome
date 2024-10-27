fbf133df9676273fc49b6b5412b716e40fdce5fc
vigneshxcode1 <vigneshxcode@gmail.com>
2024-10-27 22:04:36 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import img1 from '../src/img.jpg'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <img src={img1} alt="" />
+
+      <h1>COME SOON...</h1>
+    </>
+  )
+}
+
+export default App
